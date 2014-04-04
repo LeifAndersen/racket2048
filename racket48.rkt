c6ca8a76c4acfdf3de67835fc79805d1e358453d
Leif Andersen <leif@leifandersen.net>
2014-04-03 18:37:09 -0600

    Implemented part of board and tiles.

--- racket48.rkt
+++ racket48.rkt
@@ -1,9 +1,19 @@
 #lang typed/racket
 
-(define-type (Grid T) (Listof (Listof T)))
+(define-type (Grid T) (HashTable Coordinate T))
+(define-type Tile (U 0 2 4 8 16 32 64 128 256 512 1024 2048))
+(define-type Board (Grid Tile))
 (struct: Coordinate {[x : Integer]
                      [y : Integer]}
          #:transparent)
+(define-predicate tile? Tile)
+
+(: empty-board (-> Board))
+(define (empty-board) (hash))
+
+(: board-get (Board Integer Integer -> Tile))
+(define (board-get board x y)
+  (hash-ref board (Coordinate x y) (λ () 0)))
 
 (module+ main
   (display "Hello World\n"))
