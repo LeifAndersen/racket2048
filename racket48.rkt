b937238424c03a516bb4c33c62547483096c32c1
Leif Andersen <leif@leifandersen.net>
2014-04-03 19:25:05 -0600

    Added stub code.

--- racket48.rkt
+++ racket48.rkt
@@ -3,10 +3,12 @@
 (define-type (Grid T) (HashTable Coordinate T))
 (define-type Tile (U 0 2 4 8 16 32 64 128 256 512 1024 2048))
 (define-type Board (Grid Tile))
+(define-type Direction (U 'up 'down 'left 'right))
 (struct: Coordinate {[x : Integer]
                      [y : Integer]}
          #:transparent)
 (define-predicate tile? Tile)
+(define-predicate direction? Direction)
 
 (: empty-board (-> Board))
 (define (empty-board) (hash))
@@ -19,5 +21,30 @@
 (define (board-set board x y tile)
   (hash-set board (Coordinate x y) tile))
 
+;; TODO
+(: slide-tiles (Board Direction -> Board))
+(define (slide-tiles board direction)
+  board)
+
+;; TODO
+(: place-tile (Board -> Board))
+(define (place-tile board)
+  board)
+
+;; TODO
+(: step (Board -> Board))
+(define (step board)
+  board)
+
+;; TODO
+(: run (Board -> Board))
+(define (run board)
+  board)
+
+(: play-2048 (-> Board))
+(define (play-2048)
+  (define board (empty-board))
+  (run board))
+
 (module+ main
-  (display "Hello World\n"))
+  (play-2048))
